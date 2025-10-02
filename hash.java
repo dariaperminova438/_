07f0b6c13f901d8df4915b25fc899bb952c61daf
dariaperminova438 <koteika.2006@mail.ru>
2025-10-02 14:32:24 +0300

    Update hash.java

--- hash.java
+++ hash.java
@@ -1,13 +1,20 @@
-import java.util.*;
-class H<K,V> {
-    ArrayList<LinkedList<AbstractMap.SimpleEntry<K,V>>> t=new ArrayList<>();
-    H() { for(int i=0;i<10;i++) t.add(new LinkedList<>()); }
-    void p(K k,V v) { 
-        for(var e:t.get(k.hashCode()%10)) if(e.getKey().equals(k)) {e.setValue(v);return;}
-        t.get(k.hashCode()%10).add(new AbstractMap.SimpleEntry<>(k,v));
-    }
-    V g(K k) {
-        for(var e:t.get(k.hashCode()%10)) if(e.getKey().equals(k)) return e.getValue();
-        return null;
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+        // Создание хэш-таблицы для хранения стран и их столиц
+        HashMap<String, String> countryCapital = new HashMap<>();
+        
+        // Добавление элементов в хэш-таблицу
+        countryCapital.put("Russia", "Moscow");
+        countryCapital.put("Germany", "Berlin");
+        countryCapital.put("France", "Paris");
+        countryCapital.put("Japan", "Tokyo");
+        
+        // Вывод всех элементов хэш-таблицы
+        System.out.println("Countries and their capitals:");
+        for (var entry : countryCapital.entrySet()) {
+            System.out.println(entry.getKey() + " - " + entry.getValue());
+        }
     }
 }
